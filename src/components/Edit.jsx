d0ff3a7c67c04149522fb0d8ebb365d7248b8f8e
Ankitha <avankitha.10a@gmail.com>
2026-03-15 21:05:59 +0530

    initial frontend upload

---
+++ src/components/Edit.jsx
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import '../css/Home.css'
+
+export default function Edit() {
+    const navigate = useNavigate()
+    const {id} = useParams()
+    console.log(id)
+    const[name, setName] = useState('')
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/tasks/${id}`)
+        .then((res) => {
+            setName(res.data.task)
+            console.log(name)
+        })
+        .catch((error) =>
+        {
+            console.log(error)
+        })
+    },[])
+
+    const updateUser = async() => {
+        try {
+            await axios.put(`http://localhost:5000/api/tasks/${id}`, {task:name})
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+  return (
+    <div className='main'>
+    <div className='task'>
+        <h2 className='task_manager'>Edit the task</h2>
+        <input type="text" value = {name} onChange={(e) => setName(e.target.value)} />
+        <button onClick = {updateUser} className = 'btn'>update</button>
+        </div>
+    </div>
+  )
+}
